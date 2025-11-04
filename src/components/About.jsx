7c445eb48937f12102947efc1c371f2c5715e305
AldiCorrea <amelanicorrea@gmail.com>
2025-11-04 01:15:32 -0300

    Restore animation in About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import './About.css';
-// import { useTranslation } from 'react-i18next';
-// import Lottie from 'react-lottie-player';
-// import circuitAnimationData from '../assets/animations/circuit-animation.json';
+import { useTranslation } from 'react-i18next';
+import Lottie from 'react-lottie-player';
+import circuitAnimationData from '../assets/animations/circuit-animation.json';
 
 const About = () => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <section id="about">
       <div className="about-content">
-        <h2>{/*t('about.title')*/}About Me</h2>
-        <p>{/*t('about.content')*/}This is the about section.</p>
-        <p>Hello World</p>
+        <h2>{t('about.title')}</h2>
+        <p>{t('about.content')}</p>
       </div>
-      {/* <div className="circuit-animation">
+      <div className="circuit-animation">
         <Lottie
           loop
           animationData={circuitAnimationData}
           play
           style={{ width: 400, height: 300 }}
         />
-      </div> */}
+      </div>
     </section>
   );
 };
